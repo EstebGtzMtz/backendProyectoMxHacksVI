c5168974861b79ff02d5402783e9ddede0121b02
Esteban <esteb.gtz.mtz@hotmail.com>
2019-10-26 00:38:33 -0500

    first commit
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
+require('./config/config');
 const app = require('express')();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('que pedo');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/user', (req, res) => {
+    res.json('que pedo');
+});
+
+app.post('/user', (req, res) => {
+    const body = req.body;
+    res.json({
+        body
+    });
 })
 
-app.listen(3000, () => {
-    console.log('escuchando el puerto 3000');
+app.put('/user/:id', (req, res) => {
+    const id = req.param.id;
+
+    res.json('put works');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('escuchando el puerto: ', process.env.PORT);
 });
